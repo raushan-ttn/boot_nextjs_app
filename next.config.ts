8967661c8b318dc20ff3300578b79d4f4e0a1098
Raushan <raushan.tiwari@tothenew.com>
2025-03-03 21:20:14 +0530

    code update

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,19 @@
 import type { NextConfig } from "next";
+import bundleAnalyzerImport from "@next/bundle-analyzer";
 
+const withBundleAnalyzer = bundleAnalyzerImport({
+  enabled: process.env.ANALYZE === "true",
+});
 const nextConfig: NextConfig = {
+  compress: true, // Enable compression
+  reactStrictMode: true,
   /* config options here */
   publicRuntimeConfig: {
     RUNTIME_VARIABLE: process.env.RUNTIME_VARIABLE,
   },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
+
+// ANALYZE=true npm run build  // to run the bundle analyzer
+// npm run build // to build the app
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import getConfig from "next/config";
-const { publicRuntimeConfig } = getConfig();
-
 export default function Home() {
-  console.log(publicRuntimeConfig, "publicRuntimeConfig");
   return (
     <div className="min-h-[88vh]">
       <div className="">Home page</div>
       <div className="">Build Time: {process.env.BUILD_TIME_VARIABLE}</div>
-      <div className="">Run Time: {publicRuntimeConfig?.RUNTIME_VARIABLE}</div>
+      <div className="">Run Time: {process.env.RUNTIME_VARIABLE}</div>
     </div>
   );
 }
